25082449968421227c737756b43c0944b826d323
Lenarqa <lenar.valiev211@gmail.com>
2021-10-07 21:39:06 +0700

    add mnemonic connect

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,14 +4,19 @@
 
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
 //посмотреть локальные переменные prientenv
 module.exports = {
   solidity: "0.8.0",
   networks: {
+    // rinkeby: {
+    //   url: process.env.INFURA_URL,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    // }
     rinkeby: {
-      url: process.env.INFURA_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      url: process.env.INFURA_API_KEY,
+      accounts: {mnemonic: process.env.MNEMONIC},
     }
   },
 };
